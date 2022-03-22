a276d4e149b378ec684d50e1ad0cd235a3dc8725
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2022-03-22 16:27:54 +0300

    centroid assigment and documentation improvments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
 import random
 import statistics
+import copy
+
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
-import pandas as pd
 
 number_of_clusters_const = 4
 points_per_cluster_const = 150
@@ -31,13 +32,27 @@ def generate_landscape(number_of_clusters: int, points_per_cluster: int):
     return axis_x, axis_y
 
 
-def get_random_points(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
-    random_x_axis = []
-    random_y_axis = []
+def generate_random_points(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
+    random_x_axis: list[float] = []
+    random_y_axis: list[float] = []
 
     for i in range(number_of_clusters):
         random_x = random.uniform(min(x_axis), max(x_axis))
         random_y = random.uniform(min(y_axis), max(y_axis))
+        random_x_axis.append(random_x)
+        random_y_axis.append(random_y)
+
+    return random_x_axis, random_y_axis
+
+
+def pick_random_points(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
+    random_x_axis: list[float] = []
+    random_y_axis: list[float] = []
+
+    for i in range(number_of_clusters):
+        temp_index = random.randint(0, len(x_axis) - 1)
+        random_x = x_axis[temp_index]
+        random_y = y_axis[temp_index]
         random_x_axis.append(random_x)
         random_y_axis.append(random_y)
 
@@ -51,14 +66,18 @@ def kmeans(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
     point_centroid_mapping: list[int] = [0] * len(x_axis)
     centroids_x_axis: list[float]
     centroids_y_axis: list[float]
-    centroids_x_axis, centroids_y_axis = get_random_points(number_of_clusters, x_axis, y_axis)
+    centroids_x_axis, centroids_y_axis = pick_random_points(number_of_clusters, x_axis, y_axis)
 
-    attempt = 0
-    random_fix_attempt = 0
+    centroids_x_axis_by_iterations: list[list[float]] = []
+    centroids_y_axis_by_iterations: list[list[float]] = []
+    point_centroid_mapping_by_iterations: list[list[int]] = []
+
+    random_fix_attempt: int = 0
+    attempt: int = 0
     while diff:
         # for each observation
         for point_index in range(len(x_axis)):
-            mean_distance = float('inf')
+            max_distance = float('inf')
             point_x = x_axis[point_index]
             point_y = y_axis[point_index]
 
@@ -69,9 +88,13 @@ def kmeans(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
                 distance = math.sqrt((centroid_x - point_x) ** 2 + (centroid_y - point_y) ** 2)
 
                 # store closest centroid
-                if mean_distance > distance:
-                    mean_distance = distance
+                if max_distance > distance:
+                    max_distance = distance
                     point_centroid_mapping[point_index] = centroid_index
+
+        centroids_x_axis_by_iterations.append(copy.deepcopy(centroids_x_axis))
+        centroids_y_axis_by_iterations.append(copy.deepcopy(centroids_y_axis))
+        point_centroid_mapping_by_iterations.append(copy.deepcopy(point_centroid_mapping))
 
         for point_index, centroid_index in enumerate(point_centroid_mapping):
             if centroid_index not in centroid_point_x_mapping:
@@ -83,7 +106,7 @@ def kmeans(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
             centroid_point_y_mapping[centroid_index].append(y_axis[point_index])
 
         if len(centroid_point_x_mapping) != len(centroids_x_axis) and random_fix_attempt < 10:
-            centroids_x_axis, centroids_y_axis = get_random_points(number_of_clusters, x_axis, y_axis)
+            centroids_x_axis, centroids_y_axis = pick_random_points(number_of_clusters, x_axis, y_axis)
             random_fix_attempt += 1
             continue
 
@@ -113,34 +136,33 @@ def kmeans(number_of_clusters: int, x_axis: list[float], y_axis: list[float]):
             else:
                 fixed_centroid += 1
 
-        if number_of_clusters == fixed_centroid or attempt == 10:
+        if number_of_clusters == fixed_centroid or attempt == 300:
             diff = 0
         else:
             attempt += 1
 
-    return centroids_x_axis, centroids_y_axis, point_centroid_mapping
+    return centroids_x_axis_by_iterations, centroids_y_axis_by_iterations, point_centroid_mapping_by_iterations
 
 
 axis_x_result, axis_y_result = generate_landscape(number_of_clusters_const, points_per_cluster_const)
 
-centroids_x_axis_result, centroids_y_axis_result, point_centroid_mapping_result = kmeans(number_of_clusters_const,
-                                                                                         axis_x_result, axis_y_result)
+# Get results of my k-means
+centroids_x_axis_results, centroids_y_axis_results, point_centroid_mapping_results = kmeans(number_of_clusters_const,
+                                                                                            axis_x_result,
+                                                                                            axis_y_result)
 
-points = [list(a) for a in zip(axis_x_result, axis_y_result)]
+centroids_first_iter_x = centroids_x_axis_results[0]
+centroids_first_iter_y = centroids_y_axis_results[0]
+point_centroid_mapping_first_iter = point_centroid_mapping_results[0]
 
-# points = pd.DataFrame({'x': axis_x_result, 'y': axis_y_result})
+centroids_second_iter_x = centroids_x_axis_results[1]
+centroids_second_iter_y = centroids_y_axis_results[1]
+point_centroid_mapping_second_iter = point_centroid_mapping_results[1]
 
-# X, y = make_blobs(
-#     n_samples=150, n_features=2,
-#     centers=3, cluster_std=0.5,
-#     shuffle=True, random_state=0
-# )
+centroids_third_iter_x = centroids_x_axis_results[2]
+centroids_third_iter_y = centroids_y_axis_results[2]
+point_centroid_mapping_third_iter = point_centroid_mapping_results[2]
 
-km = KMeans(
-    n_clusters=number_of_clusters_const, init='random',
-    n_init=10, max_iter=300,
-    tol=1e-04, random_state=0
-)
-
-y_km = km.fit_predict(points)
-print(y_km)
+centroids_x_axis_result = centroids_x_axis_results[len(centroids_x_axis_results) - 1]
+centroids_y_axis_result = centroids_y_axis_results[len(centroids_y_axis_results) - 1]
+point_centroid_mapping_result = point_centroid_mapping_results[len(point_centroid_mapping_results) - 1]
